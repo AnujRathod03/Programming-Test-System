92e1e4550f25fe98c559e330be0784473a02eb94
AnujRathod03 <rathodanuj22@gmail.com>
2026-07-20 01:16:11 +0530

    Add files via upload
    A Python-based Programming Quiz System that allows users to take Python, Java, and DSA quizzes, calculates scores automatically, and sends the final result directly to the user's email using SMTP.

---
+++ Mcq_all.py
@@ -0,0 +1,501 @@
+#Program To Take Test And Count Numbers/marks
+python ={
+ "question" :{
+     101:{
+        "question":"Who is known as the father of Python?",
+        "option":{
+            "A":"James Bond",
+            "B":"Elon Musk",
+            "C":"Dennis Ritchie",
+            "D":"Guido Van Rossum"
+        },
+        "answer":"D"
+    },
+    102:{
+        "question":"Which keyword is used to define a function in Python?",
+        "option":{
+            "A":"function",
+            "B":"def",
+            "C":"fun",
+            "D":"define"
+        },
+        "answer":"B"
+    },
+    103:{
+        "question":"Which data type is immutable in Python?",
+        "option":{
+            "A":"List",
+            "B":"Dictionary",
+            "C":"Tuple",
+            "D":"Set"
+        },
+        "answer":"C"
+    },
+    104:{
+        "question":"Which symbol is used for comments in Python?",
+        "option":{
+            "A":"//",
+            "B":"/*",
+            "C":"#",
+            "D":"<!--"
+        },
+        "answer":"C"
+    },
+    105:{
+        "question":"Which function is used to take input in Python?",
+        "option":{
+            "A":"scan()",
+            "B":"input()",
+            "C":"read()",
+            "D":"get()"
+        },
+        "answer":"B"
+    },
+    106:{
+        "question":"Which keyword is used to create a class in Python?",
+        "option":{
+            "A":"class",
+            "B":"Class",
+            "C":"define",
+            "D":"object"
+        },
+        "answer":"A"
+    },
+    107:{
+        "question":"Which loop is not available in Python?",
+        "option":{
+            "A":"for",
+            "B":"while",
+            "C":"do-while",
+            "D":"nested for"
+        },
+        "answer":"C"
+    },
+    108:{
+        "question":"Which operator is used for exponentiation in Python?",
+        "option":{
+            "A":"^",
+            "B":"**",
+            "C":"//",
+            "D":"%%"
+        },
+        "answer":"B"
+    },
+    109:{
+        "question":"Which function returns the length of a list?",
+        "option":{
+            "A":"count()",
+            "B":"size()",
+            "C":"len()",
+            "D":"length()"
+        },
+        "answer":"C"
+    },
+    110:{
+        "question":"Python is a ______ language.",
+        "option":{
+            "A":"Compiled only",
+            "B":"Interpreted",
+            "C":"Assembly",
+            "D":"Machine"
+        },
+        "answer":"B"
+    },
+    }
+      } 
+    
+java={
+    "question" :{
+         111:{
+        "question":"Who developed Java?",
+        "option":{
+            "A":"Dennis Ritchie",
+            "B":"James Gosling",
+            "C":"Guido Van Rossum",
+            "D":"Bjarne Stroustrup"
+        },
+        "answer":"B"
+    },
+    112:{
+        "question":"Which keyword is used to inherit a class in Java?",
+        "option":{
+            "A":"inherits",
+            "B":"extends",
+            "C":"implements",
+            "D":"super"
+        },
+        "answer":"B"
+    },
+    113:{
+        "question":"Which method is the entry point of a Java program?",
+        "option":{
+            "A":"start()",
+            "B":"run()",
+            "C":"main()",
+            "D":"execute()"
+        },
+        "answer":"C"
+    },
+    114:{
+        "question":"Java is a ______ language.",
+        "option":{
+            "A":"Platform dependent",
+            "B":"Platform independent",
+            "C":"Assembly",
+            "D":"Machine"
+        },
+        "answer":"B"
+    },
+    115:{
+        "question":"Which keyword is used to create an object in Java?",
+        "option":{
+            "A":"create",
+            "B":"new",
+            "C":"object",
+            "D":"class"
+        },
+        "answer":"B"
+    },
+    116:{
+        "question":"Which package is imported by default in Java?",
+        "option":{
+            "A":"java.util",
+            "B":"java.io",
+            "C":"java.lang",
+            "D":"java.net"
+        },
+        "answer":"C"
+    },
+    117:{
+        "question":"Which symbol ends a statement in Java?",
+        "option":{
+            "A":".",
+            "B":";",
+            "C":":",
+            "D":","
+        },
+        "answer":"B"
+    },
+    118:{
+        "question":"Which keyword is used for exception handling?",
+        "option":{
+            "A":"error",
+            "B":"catch",
+            "C":"try",
+            "D":"Both C and B"
+        },
+        "answer":"D"
+    },
+    119:{
+        "question":"Which of these is not a Java primitive data type?",
+        "option":{
+            "A":"int",
+            "B":"float",
+            "C":"String",
+            "D":"char"
+        },
+        "answer":"C"
+    },
+    120:{
+        "question":"Java bytecode runs on ______.",
+        "option":{
+            "A":"Compiler",
+            "B":"JVM",
+            "C":"CPU",
+            "D":"IDE"
+        },
+        "answer":"B"
+
+      }
+      }
+}
+ 
+dsa={
+    "question" :{
+          121:{
+        "question":"DSA stands for?",
+        "option":{
+            "A":"Data Structure and Algorithm",
+            "B":"Digital System Algorithm",
+            "C":"Data Software Application",
+            "D":"Data Science Analysis"
+        },
+        "answer":"A"
+    },
+    122:{
+        "question":"Which data structure follows FIFO?",
+        "option":{
+            "A":"Stack",
+            "B":"Queue",
+            "C":"Tree",
+            "D":"Graph"
+        },
+        "answer":"B"
+    },
+    123:{
+        "question":"Which data structure follows LIFO?",
+        "option":{
+            "A":"Queue",
+            "B":"Linked List",
+            "C":"Stack",
+            "D":"Heap"
+        },
+        "answer":"C"
+    },
+    124:{
+        "question":"Which searching algorithm works only on sorted arrays?",
+        "option":{
+            "A":"Linear Search",
+            "B":"Binary Search",
+            "C":"DFS",
+            "D":"BFS"
+        },
+        "answer":"B"
+    },
+    125:{
+        "question":"What is the time complexity of Binary Search?",
+        "option":{
+            "A":"O(n)",
+            "B":"O(log n)",
+            "C":"O(n^2)",
+            "D":"O(1)"
+        },
+        "answer":"B"
+    },
+    126:{
+        "question":"Which traversal uses a Queue?",
+        "option":{
+            "A":"DFS",
+            "B":"BFS",
+            "C":"Inorder",
+            "D":"Postorder"
+        },
+        "answer":"B"
+    },
+    127:{
+        "question":"Which traversal uses a Stack?",
+        "option":{
+            "A":"BFS",
+            "B":"DFS",
+            "C":"Level Order",
+            "D":"None"
+        },
+        "answer":"B"
+    },
+    128:{
+        "question":"Which sorting algorithm has the best average time complexity?",
+        "option":{
+            "A":"Bubble Sort",
+            "B":"Selection Sort",
+            "C":"Quick Sort",
+            "D":"Linear Search"
+        },
+        "answer":"C"
+    },
+    129:{
+        "question":"Which data structure is used for recursion?",
+        "option":{
+            "A":"Queue",
+            "B":"Stack",
+            "C":"Tree",
+            "D":"Graph"
+        },
+        "answer":"B"
+    },
+    130:{
+        "question":"Which traversal of a Binary Search Tree gives sorted output?",
+        "option":{
+            "A":"Preorder",
+            "B":"Postorder",
+            "C":"Inorder",
+            "D":"Level Order"
+        },
+        "answer":"C"
+    }
+}
+}
+Name=input("Enter You Name : ")
+receiver_email = input("Enter Receiver Email : ")
+print("-"*35)
+
+print("Welcome To The Test Center")
+print("Press 1 To Give Python Test")
+print("Press 2 To Give Java Test")
+print("Press 3 To Give DSA Test")
+def show_question(question): 
+    score=0
+    for key,value in question.items():
+          print("Question.",key)
+          print(value["question"])
+          
+          for option,text in value["option"].items():
+               print(option,":",text)
+          
+          user=input("Enter the answer(A/B/C/D) : ").upper()
+          
+          if user==value["answer"]:
+            print("Correct answer")  
+            score+=1
+         
+          else:
+            print("Wrong Answer")
+            print("The Correct answer is ",value["answer"])
+     
+    return score
+
+
+
+# result=show_question()
+ch=int(input("Enter Your Subject Choice(1/2/3) : "))
+match ch:
+    case 1:
+        result=show_question(python["question"])
+        print("*"*40)
+        print("Your Score ",result,"/ 10")
+        
+        
+        import random
+        import smtplib
+        from email.mime.text import MIMEText
+        from email.mime.multipart import MIMEMultipart
+
+# Generate OTP
+        otp = result,"/10"
+
+# Sender Details
+        sender_email = "Your Email Here"
+        app_password = "Your Key"
+
+# Receiver
+        
+
+# Email Body
+        subject = "OTP Verification"
+        body = f"""
+Hello {Name},
+
+Your Result is: {otp}
+Thank You
+"""
+
+# Create Email
+        msg = MIMEMultipart()
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(body, "plain"))
+
+# SMTP
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(sender_email, app_password)
+
+        server.send_message(msg)
+
+        server.quit()
+
+        print("Result Sent To email successfully")
+
+    case 2:
+        result=show_question(java["question"])    
+        print("*"*40)
+        print("Your Score ",result,"/ 10")
+
+        import random
+        import smtplib
+        from email.mime.text import MIMEText
+        from email.mime.multipart import MIMEMultipart
+
+# Generate OTP
+        otp = result,"/10"
+
+# Sender Details
+        sender_email = "Your Email Here"
+        app_password = "Your Key"
+
+# Receiver
+        
+
+# Email Body
+        subject = "OTP Verification"
+        body = f"""
+Hello {Name},
+
+Your Result is: {otp}
+Thank You
+"""
+
+# Create Email
+        msg = MIMEMultipart()
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(body, "plain"))
+
+# SMTP
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(sender_email, app_password)
+
+        server.send_message(msg)
+
+        server.quit()
+
+        print("Result Sent To email successfully")
+    case 3:
+        result=show_question(dsa["question"])    
+        print("*"*40)
+        print("Your Score ",result,"/ 10")
+
+        import random
+        import smtplib
+        from email.mime.text import MIMEText
+        from email.mime.multipart import MIMEMultipart
+
+# Generate OTP
+        otp = result,"/10"
+
+# Sender Details
+        sender_email = "Your Email Here"
+        app_password = "Your Key"
+
+# Receiver
+        
+
+# Email Body
+        subject = "OTP Verification"
+        body = f"""
+Hello {Name},
+
+Your Result is: {otp}
+Thank You
+"""
+
+# Create Email
+        msg = MIMEMultipart()
+        msg["From"] = sender_email
+        msg["To"] = receiver_email
+        msg["Subject"] = subject
+
+        msg.attach(MIMEText(body, "plain"))
+
+# SMTP
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+
+        server.starttls()
+
+        server.login(sender_email, app_password)
+
+        server.send_message(msg)
+
+        server.quit()
+
+        print("Result Sent To email successfully")
+    case _:
+        print("Invalid Choice")  
